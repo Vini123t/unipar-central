e0580973de1ac25e6d326b2e777bae07f05552ba
Vini123t <vinicius.alm@edu.unipar.br>
2023-06-16 22:54:11 -0300

    Opcao nao encontrada

---
+++ unipar-central/src/main/java/br/unipar/central/exceptions/OpcaoNaoEncontradaException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unipar.central.exceptions;
+
+/**
+ *
+ * @author vinid
+ */
+public class OpcaoNaoEncontradaException extends Exception {
+     public OpcaoNaoEncontradaException() {
+        super("Opção não encontrada!!!!!! Verifique suas op");
+    }
+    
+}
